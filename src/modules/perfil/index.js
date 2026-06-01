90f252ffc00c7beb4d96ce7704c8cf9b874d1ffc
AntonellaMilla <antonella.milla@tecsup.edu.pe>
2026-05-31 23:12:20 -0500

    sdf

--- src/modules/perfil/index.js
+++ src/modules/perfil/index.js
@@ -1,3 +1,3 @@
 export { default as ProfilePage } from './components/ProfilePage';
 export { default as EditProfilePage } from './components/EditProfilePage';
-export { default as perfilService } from './services/perfilService';
+export { perfilService } from './services/perfilService.js';
